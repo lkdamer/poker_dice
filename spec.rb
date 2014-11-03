773da77cb0265001adb7038b3afb47c10e838697
Linnea <lkdamer@gmail.com>
2014-11-03 13:35:57 -0800

    got first test working

--- spec.rb
+++ spec.rb
@@ -1,6 +1,7 @@
 require_relative 'die'
 
 describe Die do
-  specify "roll sets @facevalue to one of FACES"
-    expect( Die.roll ).to eq()
+  specify "roll sets @facevalue to one of FACES" do
+    expect( Die.new.top_face ).to match( /[9TJQKA]/ )
+  end
 end
