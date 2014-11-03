28f05870dacf6f8b84b0655cdb0205c026717aa8
Linnea <lkdamer@gmail.com>
2014-11-03 13:20:52 -0800

    initial commit

---
+++ die.rb
@@ -0,0 +1,15 @@
+class Die
+  FACES = %w[ 9 T J Q K A ]
+  # %w is ruby shorthand for an array of strings ~= ['9', 'T', 'J', 'Q', 'K', 'A']
+
+  attr_reader :top_face
+
+  def initialize
+    roll
+  end
+
+  def roll
+    @top_face = FACES.sample #[ rand(0..5) ] also can
+  end
+
+end
---
+++ spec.rb
@@ -0,0 +1,6 @@
+require_relative 'die'
+
+describe Die do
+  specify "roll sets @facevalue to one of FACES"
+    expect( Die.roll ).to eq()
+end
